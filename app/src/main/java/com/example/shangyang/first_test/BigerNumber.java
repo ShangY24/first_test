c85745c49f3df958d16b961d138e83a85879e84c
Yang Shang <shang.yang@rutgers.edu>
2016-09-13 12:01:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/shangyang/first_test/BigerNumber.java
@@ -0,0 +1,13 @@
+package com.example.shangyang.first_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BigerNumber extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_biger_number);
+    }
+}
